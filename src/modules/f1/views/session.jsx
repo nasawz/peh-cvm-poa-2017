8e56fbfd6ca012b54635da7f7dbb8f5f287eeeca
王喆 <nasa127@gmail.com>
2016-11-14 01:11:49 +0800

    fix

--- src/modules/f1/views/session.jsx
+++ src/modules/f1/views/session.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import classNames from 'classnames'
 
 const Session = React.createClass({
     componentDidMount() {
@@ -6,15 +7,23 @@ const Session = React.createClass({
         xscroll.render()
     },
     render() {
+        let _style = {
+            fontSize: window.innerWidth+'px',
+        }
+        let classes = {
+            'xs-content':true,
+            'iphone-plus':(window.innerWidth>400)
+        }
         return (
             <div className='poa-session' id='poa-session' ref='session'>
                 <div className="xs-container">
-                    <div className="xs-content">
+                    <div className={classNames(classes)} style={_style}>
                         <img className='q' src={require('../../../img/q.png')}/>
                         <img className='q1-2' src={require('../../../img/q1-2.png')}/>
                         <img className='q1-3' src={require('../../../img/q1-3.png')}/>
                         <img className='q2-2' src={require('../../../img/q2-2.png')}/>
                         <img className='q2-3' src={require('../../../img/q2-3.png')}/>
+                        <br />
                     </div>
                 </div>
             </div>
