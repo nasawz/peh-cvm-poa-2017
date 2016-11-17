fa4e93db13835e21f977722f8591c74b21889346
李继红 <707230595@qq.com>
2016-11-17 16:24:02 +0800

    fix

--- src/modules/f3/views/session.jsx
+++ src/modules/f3/views/session.jsx
@@ -20,20 +20,8 @@ const Session = React.createClass({
             <div className='poa-session' id='poa-session' ref='session'>
                 <div className="xs-container">
                     <div className={classNames(classes)} style={_style}>
-                        <img className='q1-2' src={require('../../../img/f3-p1.png')}/>
-                        <img className='q1-3' src={require('../../../img/f3-p2.png')}/>
-                        <img className='q1-3' src={require('../../../img/f3-p3.png')}/>
-                        <img className='q1-3' src={require('../../../img/f3-p4.png')}/>
-                        <img className='q1-2' src={require('../../../img/f3-p5.png')}/>
-                        <img className='q' src={require('../../../img/f3-p6.png')}/>
-                        <img className='q1-3' src={require('../../../img/f3-p7.png')}/>
-                        <img className='q1-3' src={require('../../../img/f3-p8.png')}/>
-                        <img className='q1-3' src={require('../../../img/f3-p9.png')}/>
-                        <img className='q1-3' src={require('../../../img/f3-p10.png')}/>
-                        <img className='q1-3' src={require('../../../img/f3-p11.png')}/>
-                        <img className='q1-3' src={require('../../../img/f3-p12.png')}/>
-                        <img className='q1-3' src={require('../../../img/f3-p13.png')}/>
-                        <img className='q1-3' src={require('../../../img/f3-p14.png')}/>
+                        <img className='f3-p1' src={require('../../../img/f3-p1.png')}/>
+                        <img className='f3-p2' src={require('../../../img/f3-p2.png')}/>
                     </div>
                 </div>
             </div>
